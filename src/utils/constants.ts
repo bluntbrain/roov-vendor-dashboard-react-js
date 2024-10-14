5b6e5102386349ced5d74a5871790114a8a6e817
Ishan Lakhwani <ishan7023254133@gmail.com>
2024-10-14 13:43:35 +0700

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 const TEMP_URL = 'http://13.200.54.218'
+const BASE_URL = 'https://api.roov.co.in'
 // const TEMP_URL = 'https://roov-fashion-backend.onrender.com'
 
-export const BASE_URL = TEMP_URL
+export const BASE_URL = BASE_URL
