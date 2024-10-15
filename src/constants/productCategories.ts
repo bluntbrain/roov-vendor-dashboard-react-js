2363f0bbfe2fb1ecace03989695aff99e8a8fc87
Ishan Lakhwani <ishan7023254133@gmail.com>
2024-10-15 13:16:36 +0700

    product categories added

---
+++ src/constants/productCategories.ts
@@ -0,0 +1,42 @@
+export const productCategories = [
+  {
+    name: "Tops",
+    subcategories: ["T-shirts", "Shirts", "Blouses", "Tank Tops", "Crop Tops", "Tunics"]
+  },
+  {
+    name: "Bottoms",
+    subcategories: ["Jeans", "Trousers", "Shorts", "Skirts", "Leggings", "Cargo Pants"]
+  },
+  {
+    name: "Dresses",
+    subcategories: ["Maxi Dresses", "Mini Dresses", "Midi Dresses", "Sundresses", "Cocktail Dresses", "Formal Dresses"]
+  },
+  {
+    name: "Outerwear",
+    subcategories: ["Jackets", "Coats", "Blazers", "Sweaters", "Cardigans", "Hoodies", "Sweatshirts"]
+  },
+  {
+    name: "Ethnic Wear",
+    subcategories: ["Sarees", "Salwar Kameez", "Kurtis", "Lehengas", "Sherwanis", "Kurta Pajamas"]
+  },
+  {
+    name: "Activewear",
+    subcategories: ["Gym Wear", "Yoga Clothes", "Sports Bras", "Athletic Shorts", "Track Suits", "Swimwear"]
+  },
+  {
+    name: "Formal Wear",
+    subcategories: ["Suits", "Tuxedos", "Formal Shirts", "Formal Trousers", "Formal Skirts", "Formal Blouses"]
+  },
+  {
+    name: "Sleepwear",
+    subcategories: ["Pajamas", "Nightgowns", "Robes", "Sleep Shirts", "Loungewear"]
+  },
+  {
+    name: "Innerwear",
+    subcategories: ["Bras", "Panties", "Boxers", "Briefs", "Vests", "Thermal Wear"]
+  },
+  {
+    name: "Accessories",
+    subcategories: ["Belts", "Scarves", "Hats", "Gloves", "Socks", "Ties"]
+  }
+];
